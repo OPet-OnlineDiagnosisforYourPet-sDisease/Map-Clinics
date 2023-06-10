fc9a375fff10819a6514e1e6dfa29a11b035f52d
Fathurrahman Hafiz <134243027+fathurrahmanhfz@users.noreply.github.com>
2023-06-10 12:05:55 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -3,7 +3,7 @@ const data = require('./klinik.json');
 function getKlinik(req, res) {
     try {
         // Jika berhasil, kirim respons dengan status code 200 dan pesan success
-        res.status(200).json({ error: false, message: 'Data klinik berhasil diperoleh.', data: data.klinik });
+        res.status(200).json({ error: false, message: 'Data klinik berhasil diperoleh.', klinik: data.klinik });
     } catch (error) {
         // Jika terjadi kesalahan, kirim respons dengan status code 500 dan pesan error
         res.status(500).json({ error: true, error: 'Terjadi kesalahan dalam mendapatkan data klinik.' });
